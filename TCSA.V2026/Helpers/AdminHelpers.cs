5f187d58fb99fe0d4cf710a4d54b6190eaadb553
Pablo <thecsharpacademy@gmail.com>
2025-03-28 16:17:46 +1000

    Loading spinner

--- TCSA.V2026/Helpers/AdminHelpers.cs
+++ TCSA.V2026/Helpers/AdminHelpers.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using TCSA.V2026.Data.Curriculum;
+﻿using TCSA.V2026.Data.Curriculum;
 using TCSA.V2026.Data.Models;
 
 namespace TCSA.V2026.Helpers;
@@ -13,7 +12,7 @@ public static class AdminHelpers
             return ArticleHelper.GetArticles()
                    .FirstOrDefault(a => a.Id == activity.ProjectId)?.Title
                ?? CourseHelper.GetCourses().SelectMany(x => x.Articles)
-                   .FirstOrDefault(a => a.Id == activity.ProjectId)?.Title; 
+                   .FirstOrDefault(a => a.Id == activity.ProjectId)?.Title;
         }
 
         return "Unknown";
@@ -21,12 +20,12 @@ public static class AdminHelpers
 
     public static string GetProjectName(DashboardProject project)
     {
-            return
-                  ArticleHelper.GetArticles()
-                   .FirstOrDefault(a => a.Id == project.ProjectId)?.Title
-               ?? ProjectHelper.GetProjects()
-                   .FirstOrDefault(p => p.Id == project.ProjectId)?.Title
-               ?? CourseHelper.GetCourses().SelectMany(x => x.Articles)
-                   .FirstOrDefault(a => a.Id == project.ProjectId)?.Title;
+        return
+              ArticleHelper.GetArticles()
+               .FirstOrDefault(a => a.Id == project.ProjectId)?.Title
+           ?? ProjectHelper.GetProjects()
+               .FirstOrDefault(p => p.Id == project.ProjectId)?.Title
+           ?? CourseHelper.GetCourses().SelectMany(x => x.Articles)
+               .FirstOrDefault(a => a.Id == project.ProjectId)?.Title;
     }
 }

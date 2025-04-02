ba485b798bf75b8a1e0ee5476bb8eed24d5baee8
Pablo <thecsharpacademy@gmail.com>
2025-04-02 17:11:12 +1000

    roles

--- TCSA.V2026/Program.cs
+++ TCSA.V2026/Program.cs
@@ -51,8 +51,7 @@ builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddRoles<IdentityRole>()
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();

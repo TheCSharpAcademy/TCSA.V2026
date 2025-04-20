81a5468f426870987edb2f27d0721ac327ff3f3c
Pablo <thecsharpacademy@gmail.com>
2025-04-20 17:06:22 +1000

    Adding activity to user 1

--- TCSA.V2026/Data/SeedData.cs
+++ TCSA.V2026/Data/SeedData.cs
@@ -22,7 +22,7 @@ public static class SeedData
 
         var user1 = new ApplicationUser
         {
-            Id = "1",
+            Id = Guid.NewGuid().ToString(),
             UserName = "user1@example.com",
             NormalizedUserName = "user1@example.com",
             Email = "user1@example.com",
@@ -35,13 +35,95 @@ public static class SeedData
             ReviewedProjects = 5,
             EmailConfirmed = true,
             GithubLogin = false,
-            Level = Level.Orange
+            Level = Level.Orange,
+            DashboardProjects = new List<DashboardProject>
+            {
+                new DashboardProject
+                {
+                    ProjectId = 5,
+                    GithubUrl = string.Empty,
+                    IsCompleted = true,
+                    IsArchived = false,
+                    IsPendingNotification = false,
+                    IsPendingReview = false,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc)),
+                    DateCompleted = new DateTimeOffset(new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc)),
+                    DateRequestedChange = DateTimeOffset.MinValue,
+                },
+                new DashboardProject
+                {
+                    ProjectId = 6,
+                    GithubUrl = string.Empty,
+                    IsCompleted = true,
+                    IsArchived = false,
+                    IsPendingNotification = false,
+                    IsPendingReview = false,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 2, 0, 0, 0, DateTimeKind.Utc)),
+                    DateCompleted = new DateTimeOffset(new DateTime(2025, 1, 2, 0, 0, 0, DateTimeKind.Utc)),
+                    DateRequestedChange = DateTimeOffset.MinValue,
+                },
+                new DashboardProject
+                {
+                    ProjectId = 7,
+                    GithubUrl = string.Empty,
+                    IsCompleted = true,
+                    IsArchived = false,
+                    IsPendingNotification = false,
+                    IsPendingReview = false,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 3, 0, 0, 0, DateTimeKind.Utc)),
+                    DateCompleted = new DateTimeOffset(new DateTime(2025, 1, 3, 0, 0, 0, DateTimeKind.Utc)),
+                    DateRequestedChange = DateTimeOffset.MinValue,
+                },
+                new DashboardProject
+                {
+                    ProjectId = 8,
+                    GithubUrl = string.Empty,
+                    IsCompleted = true,
+                    IsArchived = false,
+                    IsPendingNotification = false,
+                    IsPendingReview = false,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 4, 0, 0, 0, DateTimeKind.Utc)),
+                    DateCompleted = new DateTimeOffset(new DateTime(2025, 1, 4, 0, 0, 0, DateTimeKind.Utc)),
+                    DateRequestedChange = DateTimeOffset.MinValue,
+                }
+            },
+            UserActivity = new List<AppUserActivity>
+            {
+                new AppUserActivity
+                {
+                    ProjectId = 5,
+                    ChallengeId = 0,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 1, 0, 1, 0, DateTimeKind.Utc)),
+                    ActivityType = ActivityType.ArticleRead,
+                },
+                new AppUserActivity
+                {
+                    ProjectId = 6,
+                    ChallengeId = 0,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 2, 0, 1, 0, DateTimeKind.Utc)),
+                    ActivityType = ActivityType.ArticleRead,
+                },
+                new AppUserActivity
+                {
+                    ProjectId = 7,
+                    ChallengeId = 0,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 3, 0, 1, 0, DateTimeKind.Utc)),
+                    ActivityType = ActivityType.ArticleRead,
+                },
+                new AppUserActivity
+                {
+                    ProjectId = 8,
+                    ChallengeId = 0,
+                    DateSubmitted = new DateTimeOffset(new DateTime(2025, 1, 4, 0, 1, 0, DateTimeKind.Utc)),
+                    ActivityType = ActivityType.ArticleRead,
+                }
+            }
         };
         user1.PasswordHash = hasher.HashPassword(user1, "Password123!");
 
         var user2 = new ApplicationUser
         {
-            Id = "2",
+            Id = Guid.NewGuid().ToString(),
             UserName = "user2@example.com",
             NormalizedUserName = "user2@example.com",
             Email = "user2@example.com",

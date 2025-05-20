d6c547c033f2e1cd999e64dc5d722f8bbc4bc44a
thecsharpacademy <thecsharpacademy@gmail.com>
2025-05-20 16:56:14 +1000

    Main page start

---
+++ TCSA.V2026/Data/DTOs/IndexAreaInfo.cs
@@ -0,0 +1,13 @@
+﻿using TCSA.V2026.Data.Models;
+
+namespace TCSA.V2026.Data.DTOs;
+
+public class IndexAreaInfo
+{
+    public Area Area { get; set; }
+    public string Description { get; set; }
+    public string TitleImgUrl { get; set; }
+    public string Id { get; set; }
+
+    public List<Project> Projects { get; set; }
+}

e75bafe1e6270fbd1c9e43c1a270adac7415c4d3
Dejmenek <dejmen14567@gmail.com>
2025-06-07 13:38:55 +0200

    test: add FlagHelperTests for null country case
    Added a new test class `FlagHelperTests` in the
    `TCSA.V2026.UnitTests.Helpers` namespace. This class
    includes a test method `GetFlag_ShouldReturnEarthImage_WhenCountryIsNullOrEmpty`,
    which verifies that when the `country` parameter is null,
    the `FlagHelper.GetFlag` method returns the path to
    the earth image ("img/flags/earth.png").

---
+++ TCSA.V2026.UnitTests/Helpers/FlagHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using TCSA.V2026.Helpers;
+
+namespace TCSA.V2026.UnitTests.Helpers;
+
+[TestFixture]
+public class FlagHelperTests
+{
+    public void GetFlag_ShouldReturnEarthImage_WhenCountryIsNullOrEmpty()
+    {
+        // Arrange
+        string country = null;
+
+        // Act
+        var result = FlagHelper.GetFlag(country);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("img/flags/earth.png"));
+    }
+}

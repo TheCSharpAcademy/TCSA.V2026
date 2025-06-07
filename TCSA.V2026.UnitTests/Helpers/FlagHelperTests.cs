bbea1c48be0e4f1cead654eafa40531095992408
Dejmenek <dejmen14567@gmail.com>
2025-06-07 13:43:28 +0200

    test: add test for country not in the list

--- TCSA.V2026.UnitTests/Helpers/FlagHelperTests.cs
+++ TCSA.V2026.UnitTests/Helpers/FlagHelperTests.cs
@@ -5,10 +5,24 @@ namespace TCSA.V2026.UnitTests.Helpers;
 [TestFixture]
 public class FlagHelperTests
 {
+    [Test]
     public void GetFlag_ShouldReturnEarthImage_WhenCountryIsNullOrEmpty()
     {
         // Arrange
         string country = null;
+
+        // Act
+        var result = FlagHelper.GetFlag(country);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("img/flags/earth.png"));
+    }
+
+    [Test]
+    public void GetFlag_ShouldReturnEarthImage_WhenCountryNotFound()
+    {
+        // Arrange
+        string country = "Unknown Country";
 
         // Act
         var result = FlagHelper.GetFlag(country);

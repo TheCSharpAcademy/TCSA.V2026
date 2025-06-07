80e0d012479d4006b1706ba53bd82576f1b8adf6
Dejmenek <dejmen14567@gmail.com>
2025-06-07 13:45:04 +0200

    test: add tests for valid countries
    These tests verify that the method returns the correct flag image path for Canada, the United Kingdom, the United States, and Brazil.

--- TCSA.V2026.UnitTests/Helpers/FlagHelperTests.cs
+++ TCSA.V2026.UnitTests/Helpers/FlagHelperTests.cs
@@ -30,4 +30,17 @@ public class FlagHelperTests
         // Assert
         Assert.That(result, Is.EqualTo("img/flags/earth.png"));
     }
+
+    [TestCase("Canada", "img/flags/canada.png")]
+    [TestCase("United Kingdom", "img/flags/united-kingdom.png")]
+    [TestCase("United States", "img/flags/united-states.png")]
+    [TestCase("Brazil", "img/flags/brazil.png")]
+    public void GetFlag_ShouldReturnCorrectFlagImagePath_WhenCountryExists(string country, string expected)
+    {
+        // Act
+        var result = FlagHelper.GetFlag(country);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }

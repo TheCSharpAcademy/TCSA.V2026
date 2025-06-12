9a1f39cabf7a99e7736e7ce5c70ed69aaef15a50
thecsharpacademy <thecsharpacademy@gmail.com>
2025-06-12 16:09:29 +1000

    Dashboard toolbar notification start

---
+++ TCSA.V2026/Data/DTOs/TCSANotificationItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TCSA.V2026.Data.DTOs;
+
+public class TCSANotificationItem
+{
+    public string Description { get; set; }
+    public string IconUrl { get; set; }
+    public bool IsActive { get; set; }
+}

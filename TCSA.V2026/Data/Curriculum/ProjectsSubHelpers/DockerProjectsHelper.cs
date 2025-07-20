ae0d680fffd6790d2e1f3eedd2e12f3fbd38e785
thecsharpacademy <thecsharpacademy@gmail.com>
2025-07-20 19:49:49 +1000

    docker project 1, banners, repository link

--- TCSA.V2026/Data/Curriculum/ProjectsSubHelpers/DockerProjectsHelper.cs
+++ TCSA.V2026/Data/Curriculum/ProjectsSubHelpers/DockerProjectsHelper.cs
@@ -25,116 +25,56 @@ public class DockerProjectsHelper
                 IconUrl = "icons8-container-64.png",
                 BannerUrl = "intro-docker-banner.jpg",
                 Slug = "intro-docker",
+                LanguageHeadings = new EnglishHeadings(),
                 Description = "Make your applications able to run anywhere with Docker containers",
                 Area = Area.Docker,
                 Level = Level.Black,
                 ExperiencePoints = 50,
                 DisplayOrder= 1,
-                RepositoryLink = "https://github.com/TheCSharpAcademy/CodeReviews.Console.MathGame",
+                RepositoryLink = "https://github.com/TheCSharpAcademy/CodeReviews.Docker.Containers",
                 Introduction = new List<Paragraph>
                 {
                     new Paragraph
                     {
-                        Body="In your first project you’ll build a very simple Math Game app. If this is your first time building a project indepentently, I recommend making use of the C# Academy's <a target='blank' href='https://www.youtube.com/watch?v=YUc5NZZVtjA&list=PL4G0MUH8YWiD1p5ySamqNWAaWlnwp1Vip'> series from our C# Foundation area</a>.This series will guide you through the process of creating a console app from the ground up, while explaining crucial C# concepts. If you have already watched it, or if you have prior experience creating C# applications, this project will be a breeze. But it’s still helpful to reinforce your knowledge."
+                        Body="Welcome to the your first Docker project! One of the coolest latest concepts popularized in the world of software development is containerization, a technique for bundling an application along with all its necessary dependencies and configurations into a single, self-contained unit: the so-called container. This container can run reliably across different computing environments, whether it's on a developer’s laptop, a test server, or in production on the cloud."
                     },
                     new Paragraph
                     {
-                        Body="You'll find challenges at the end of this project and all other projects in The C# Academy. These challenges aren't requirements. Your project will still be approved and you'll gain the correspondent experience points. However, the challenges will reinforce what you've learned. Although you might not get extra points for completing them, you'll become a better developer each time you finish a challenge."
+                        Body="What makes it powerful is that containers share the host system's operating system kernel, which makes them lightweight and fast compared to traditional virtual machines. They ensure that \"it works on my machine\" becomes a thing of the past."
+                    },
+                    new Paragraph
+                    {
+                        Body="Docker is the tool that pushed containerization from a niche concept into the mainstream of modern development. With intuitive commands and a vast ecosystem, Docker allows developers to create, manage, and deploy containers effortlessly. Whether you're building microservices or deploying full-stack applications, Docker simplifies the process and empowers teams to iterate quickly with confidence."
                     }
+
                 },
                 Requirements = new List<string>
                 {
-                    @"You need to create a Math game containing the 4 basic operations",
-                    @"The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.",
-                    @"Users should be presented with a menu to choose an operation",
-                    @"You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.",
-                    @"You don't need to record results on a database. Once the program is closed the results will be deleted."
+                    @"You need to containerize a console app with Docker.",
+                    @"The goal is to demonstrate a working application inside a container. You do NOT need to use external databases, volumes, or advanced Docker features. The Math Game is ideal for this project, since it doesn't contain any dependencies.",
+                    @"You must create a Dockerfile that defines how your application is containerized. Include instructions to restore, build, and run your project.",
+                    @"Your Docker image should be buildable using the 'docker build' command, and the container should be runnable with 'docker run'.",
+                    @"You should include a brief README explaining how to build and run your container. Mention any dependencies or setup steps.",
                 },
                 Resources = new List<string>
                 {
-                    "<a target='_blank' href='https://www.youtube.com/watch?v=iLByqqLGYEc&list=PL4G0MUH8YWiD1p5ySamqNWAaWlnwp1Vip'>Math Game Tutorial with Visual Studio</a>",
-                    "<a target='_blank' href='https://www.youtube.com/watch?v=nnDNeJX-uBE'>Math Game Tutorial with Visual Studio Code</a>",
+
+                    "<a target='_blank' href='https://learn.microsoft.com/en-us/dotnet/core/docker/build-container?tabs=windows&pivots=dotnet-9-0'>Microsoft Tutorial: Containerize a .NET app</a>",
+                    "<a target='_blank' href='https://www.youtube.com/watch?v=HvNR1LHxboM'>Youtube: C# Docker Console Application (short video)</a>",
+                    "<a target='_blank' href='https://www.youtube.com/watch?v=cWMztQwIQNs'>Youtube: Docker Full Course (in-depth video)</a>",
                 },
-                ResourcesIntro = "If you have previous C# experience, you might be able to complete this project without any help. If you don't, here are two tutorials you can follow if you get stuck. The first is using Visual Studio and not only creates a console app, but also a Desktop version, using .NET MAUI. The second uses Visual Studio Code and might be more suitable if you prefer it or don't have access to Visual Studio. ",
-                ResourcesConclusion = "You don't need to watch the second part of this tutorial (Building a .NET Maui app), to complete this project. Only the Console App is required",
                 Tips = new List<string>
                 {
-                    "Don't forget to ALWAYS, I mean ALWAYS have your code saved on Github (or whatever source control you use). This way you won't lose your work, and you can always refer back to it in the future. You're building a library of your own!",
-                    "Don't rush through the steps, even if this tutorial is easy for you. Make sure you understand EACH WORD of the code. Leave no stone unturned.",
-                    "If there's anything you don't understand, hover over the code, and you'll get an explanation of what the term means. If you still can't understand it, google is your best friend. You'll find explanations about pretty much everything in forums like stackoverflow.com.",
-                    "If this is your first project, I recommend doing it twice. You'll be surprised at how much your retention increases, and how many gaps were left in the first time.",
-                    $"Upon completion of your project, go to our <a target='_blank' href='{DiscordLink}'>Discord community</a> and brag about it! It's a very important step to complete a project and you should celebrate."
+                    "You'll realise it's easy to get a container going without really understanding what's going on. Make sure you write notes as you go and write down questions that will inevitably arise. Use that list of questions to close knowledge gaps. It will serve as a rich reference in the future.",
+                    "Don't rush through the steps, make sure you understand each step. Write your thought process (including doubts) in your Readme.",
+                    "It's ok to use Docker Desktop at first, but if you do, we recommend you replicate the same actions using the CLI.",
+                    "Make sure you REALLY understand what each of these are: Container, Image, Container Registry. How does a Container Relate to an Image?",
+                    "On the same token, don't move on before understanding each line of your Docker file."
                 },
                 Challenges = new List<string>
                 {
-                    "Try to implement levels of difficulty.",
-                    "Add a timer to track how long the user takes to finish the game.",
-                    "Create a 'Random Game' option where the players will be presented with questions from random operations",
-                    "To follow the <a href='/article/30006/dry-principle-csharp' target='_blank'>DRY Principle</a>, try using just one method for all games. Additionally, double check your project and try to find opportunities to achieve the same functionality with less code, avoiding repetition when possible."
-                },
-                Blocks = new List<Block>
-                {
-                    new Block
-                    {
-                        Title = "AI Challenge",
-                        ImgUrl= "icons8-artificial-intelligence-64.png",
-                        Paragraphs = new List<Paragraph>
-                        {
-                            new Paragraph
-                            {
-                                Body = "This is a slightly more advanced project but it taps into the future of programming: Artificial intelligence. Can you make it so the users of your game are able to reply to the game's questions using their voice? For this you'll use Azure's Language Services. You can find a step-by-step tutorial below:"
-                            },
-                            new Paragraph
-                            {
-                                IsVideo = true,
-                                VideoUrl = "https://www.youtube.com/embed/XMJS-eQ4Y48?si=Bwbn0UZUe-thB8md"
-                            }
-                        }
-                    },
-                    new Block
-                    {
-                        Title = "Source Control",
-                        ImgUrl = "icons8-git-96.png",
-                        Paragraphs = new List<Paragraph>
-                        {
-                            new Paragraph
-                            {
-                                Body = "If this is your first tutorial, you might be tempted to skip this step and start coding. Don’t do it or you’ll learn the hard way. One day after working for many hours on a difficult problem you won’t save your work on a source control repository and for whatever reason all of it will be lost."
-                            },
-                            new Paragraph
-                            {
-                                Body = "Get into the habit of saving every line of code you ever write. I promise it will save you from many headaches. All serious developers save their work on a backup repository. The most commonly used is Github. Create an account if you haven’t yet. "
-                            },
-                            new Paragraph
-                            {
-                                Body="1. In Visual Studio, go to Git > Create Git Repository<br>2. Enter your Login Details<br>3. Click on Create and Push. "
-                            },
-                            new Paragraph
-                            {
-                                Body="✅ Done! Your repository is good to go. Don’t forget to commit and push your changes after every step!"
-                            }, new Paragraph
-                            {
-                                Body="You can take further step and learn how to integrate Github with Visual Studio watching this tutorial:"
-                            },
-                            new Paragraph
-                            {
-                                IsVideo = true,
-                                VideoUrl = "https://www.youtube.com/embed/oCvb-Q5lXb8"
-                            }
-                        }
-                    },
-                    new Block
-                    {
-                        Title = "Code Review",
-                        ImgUrl= "icons8-code-review-64.png",
-                        Paragraphs = new List<Paragraph>
-                        {
-                            new Paragraph
-                            {
-                                Body = "If you want your code to be reviewed by a member of the C# Academy, instead of creating your own repository, <a href='article/52/code-reviews' target='blank'>follow this article</a>, where you’ll learn how to create a fork from our base review repository."
-                            }
-                        }
-                    },
+                    "Update your Dockerfile to accept command-line arguments when running the container. For example, the user could pass a difficulty level or username to the math game.",
+                    "Tag your Docker image with a custom version label (e.g. 'mathgame:v1') and use that tag when running the container. This helps build awareness of image versioning and future maintainability."
                 }
             },
         };

ca83cbcff07cea006c3f64e8692d1d79cf2660b3
nwdorian <neuwirthdorian@gmail.com>
2025-08-13 23:03:15 +0200

    add BeltGifs and DiscordRoles constants

---
+++ TCSA.V2026/Helpers/Constants/DiscordRoles.cs
@@ -0,0 +1,15 @@
+namespace TCSA.V2026.Helpers.Constants;
+
+public class DiscordRoles
+{
+    public static readonly ulong Green = 1142605288311095406;
+    public static readonly ulong OliveGreen = 1142610832996433960;
+    public static readonly ulong Yellow = 1142605941347459133;
+    public static readonly ulong Orange = 1142444454674972772;
+    public static readonly ulong Red = 1208932705128153118;
+    public static readonly ulong Purple = 1142436719497592842;
+    // public static readonly ulong Brown = 
+    public static readonly ulong Grey = 1239842389875490858;
+    // public static readonly ulong Blue = 
+    public static readonly ulong Black = 1293458727755841607;
+}

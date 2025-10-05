1496898d30004bb071c6f97bf5509a928530dc1e
Dejmenek <dejmen14567@gmail.com>
2025-10-05 22:17:16 +0200

    test: add other area cases for CheckIfAreaIsCompleted method

--- TCSA.V2026.UnitTests/Helpers/DashboardProjectsHelpersTests.cs
+++ TCSA.V2026.UnitTests/Helpers/DashboardProjectsHelpersTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using TCSA.V2026.Data.Curriculum;
 using TCSA.V2026.Data.Helpers;
 using TCSA.V2026.Data.Models;
 using TCSA.V2026.Helpers;
@@ -109,5 +110,59 @@ public class DashboardProjectsHelpersTests
 
         // Assert
         Assert.That(url, Is.EqualTo($"project/{taskId}/{taskSlug}"));
+    }
+
+    [TestCase(Area.React, new[] { (int)ArticleName.ExternalApi, (int)ArticleName.ShoppingList, (int)ArticleName.FriendsManager })]
+    [TestCase(Area.StartApplying, new[] { (int)ArticleName.Resume })]
+    [TestCase(Area.SQL, new[] { (int)ArticleName.Sql2, (int)ArticleName.Sql3 })]
+    [TestCase(Area.MVC, new[] { (int)ArticleName.TodoList, (int)ArticleName.Budget })]
+    [TestCase(Area.Auth, new[] { (int)ArticleName.ProductManagement, (int)ArticleName.ExternalAuth })]
+    [TestCase(Area.Angular, new[] { (int)ArticleName.CoffeeTracker, (int)ArticleName.SleepTracker, (int)ArticleName.Quizgame })]
+    [TestCase(Area.Blazor, new[] { (int)ArticleName.MemoryGame, (int)ArticleName.FoodJournal, (int)ArticleName.SportsStatistics })]
+    [TestCase(Area.MAUI, new[] { (int)ArticleName.Books, (int)ArticleName.Monkeys, (int)ArticleName.Warehouse })]
+    [TestCase(Area.Azure, new[] { (int)ArticleName.DeployFullStack, (int)ArticleName.DeployFramework, (int)ArticleName.AzureFunctions })]
+    [TestCase(Area.Docker, new int[0])]
+    [TestCase(Area.MonthlyChallenge, new int[0])]
+    [TestCase(Area.GraduationProject, new int[0])]
+    public void CheckIfAreaIsCompleted_ShouldReturnFalse_WhenOtherAreaThanStartHereIsNotCompleted(
+        Area area,
+        int[] completedProjects)
+    {
+        // Arrange
+        var articles = ArticleHelper.GetArticles();
+        var projects = ProjectHelper.GetProjects().Where(x => x.IsClosed == false);
+
+        // Act
+        var isCompleted = DashboardProjectsHelpers.CheckIfAreaIsCompleted(projects, articles, completedProjects.ToList(), area, null);
+
+        // Assert
+        Assert.That(isCompleted, Is.False);
+    }
+
+    [TestCase(Area.React, new[] { (int)ArticleName.TicTacToe, (int)ArticleName.ExternalApi, (int)ArticleName.ShoppingList, (int)ArticleName.FriendsManager })]
+    [TestCase(Area.StartApplying, new[] { (int)ArticleName.Portfolio, (int)ArticleName.Resume })]
+    [TestCase(Area.SQL, new[] { (int)ArticleName.Sql1, (int)ArticleName.Sql2, (int)ArticleName.Sql3 })]
+    [TestCase(Area.MVC, new[] { (int)ArticleName.WaterLogger, (int)ArticleName.Movies, (int)ArticleName.TodoList, (int)ArticleName.Budget })]
+    [TestCase(Area.Auth, new[] { (int)ArticleName.Auth, (int)ArticleName.ProductManagement, (int)ArticleName.ExternalAuth })]
+    [TestCase(Area.Angular, new[] { (int)ArticleName.TourOfHeroes, (int)ArticleName.CoffeeTracker, (int)ArticleName.SleepTracker, (int)ArticleName.Quizgame })]
+    [TestCase(Area.Blazor, new[] { (int)ArticleName.WardrobeInventory, (int)ArticleName.MemoryGame, (int)ArticleName.FoodJournal, (int)ArticleName.SportsStatistics })]
+    [TestCase(Area.MAUI, new[] { (int)ArticleName.MauiMath, (int)ArticleName.Books, (int)ArticleName.Monkeys, (int)ArticleName.Warehouse })]
+    [TestCase(Area.Azure, new[] { (int)ArticleName.DeploySimpleApp, (int)ArticleName.DeployFullStack, (int)ArticleName.DeployFramework, (int)ArticleName.AzureFunctions })]
+    [TestCase(Area.Docker, new[] { (int)ArticleName.DockerIntro })]
+    [TestCase(Area.MonthlyChallenge, new[] { (int)ArticleName.BreweryApi })]
+    [TestCase(Area.GraduationProject, new[] { (int)ArticleName.Flagship })]
+    public void CheckIfAreaIsCompleted_ShouldReturnTrue_WhenOtherAreaThanStartHereIsCompleted(
+        Area area,
+        int[] completedProjects)
+    {
+        // Arrange
+        var articles = ArticleHelper.GetArticles();
+        var projects = ProjectHelper.GetProjects().Where(x => x.IsClosed == false);
+
+        // Act
+        var isCompleted = DashboardProjectsHelpers.CheckIfAreaIsCompleted(projects, articles, completedProjects.ToList(), area, null);
+
+        // Assert
+        Assert.That(isCompleted, Is.True);
     }
 }

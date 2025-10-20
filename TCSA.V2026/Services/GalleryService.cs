f731fc70082ef1938e550b89bd3eae64e7c602d8
Dejmenek <dejmen14567@gmail.com>
2025-10-20 23:01:43 +0200

    feat: add GalleryService for managing showcase items
    This commit introduces a new `GalleryService` class that implements the `IGalleryService` interface. The service includes methods for adding, deleting, and retrieving showcase items in a paginated format. It leverages Entity Framework Core for database interactions, ensuring proper exception handling and response management. Necessary namespaces have also been imported to support the new functionality.

---
+++ TCSA.V2026/Services/GalleryService.cs
@@ -0,0 +1,137 @@
+using Microsoft.EntityFrameworkCore;
+using TCSA.V2026.Data;
+using TCSA.V2026.Data.DTOs;
+using TCSA.V2026.Data.Models;
+using TCSA.V2026.Data.Models.Responses;
+using TCSA.V2026.Helpers;
+using TCSA.V2026.Helpers.Constants;
+
+namespace TCSA.V2026.Services;
+
+public interface IGalleryService
+{
+    Task<PaginatedList<ShowcaseItemDTO>?> GetItems(int pageNumber, List<int> projectIds);
+    Task<BaseResponse> AddItem(ShowcaseItemDTO newItem);
+    Task<BaseResponse> DeleteItem(ShowcaseItemDTO itemToDelete);
+}
+
+public class GalleryService : IGalleryService
+{
+    private readonly IDbContextFactory<ApplicationDbContext> _factory;
+
+    public GalleryService(IDbContextFactory<ApplicationDbContext> factory)
+    {
+        _factory = factory;
+    }
+
+    public async Task<BaseResponse> AddItem(ShowcaseItemDTO newItem)
+    {
+        var response = new BaseResponse
+        {
+            Status = ResponseStatus.Success,
+            Message = "Item added successfully"
+        };
+
+        var showcaseItem = new ShowcaseItem
+        {
+            DashboardProjectId = newItem.DashboardProjectId,
+            AppUserId = newItem.ApplicationUserId,
+            VideoUrl = newItem.VideoUrl,
+            GithubUrl = newItem.GithubUrl,
+        };
+
+        try
+        {
+            using var context = _factory.CreateDbContext();
+            await context.ShowcaseItems.AddAsync(showcaseItem);
+            var result = await context.SaveChangesAsync();
+
+            if (result == 0)
+            {
+                response.Status = ResponseStatus.Fail;
+                response.Message = "Item could not be added (no changes made).";
+            }
+
+            newItem.Id = showcaseItem.Id;
+            response.Data = newItem;
+        }
+        catch (Exception ex)
+        {
+            response = new BaseResponse
+            {
+                Status = ResponseStatus.Fail,
+                Message = $"An error occurred while adding the item: {ex.Message}"
+            };
+        }
+
+        return response;
+    }
+
+    public async Task<BaseResponse> DeleteItem(ShowcaseItemDTO itemToDelete)
+    {
+        var response = new BaseResponse
+        {
+            Status = ResponseStatus.Success,
+            Message = "Project deleted successfully"
+        };
+
+        try
+        {
+            using var context = _factory.CreateDbContext();
+            var showcaseItem = await context.ShowcaseItems.FirstOrDefaultAsync(x => x.Id == itemToDelete.Id);
+            context.ShowcaseItems.Remove(showcaseItem);
+
+            var result = await context.SaveChangesAsync();
+            if (result == 0)
+            {
+                response.Status = ResponseStatus.Fail;
+                response.Message = "Project could not be deleted (not found or no changes).";
+            }
+        }
+        catch (Exception ex)
+        {
+            response = new BaseResponse
+            {
+                Status = ResponseStatus.Fail,
+                Message = $"An error occurred while deleting the project: {ex.Message}"
+            };
+        }
+
+        return response;
+    }
+
+    public async Task<PaginatedList<ShowcaseItemDTO>?> GetItems(int pageNumber, List<int> projectIds)
+    {
+        using var context = _factory.CreateDbContext();
+        try
+        {
+            var query = context.ShowcaseItems
+                .Include(x => x.ApplicationUser)
+                .Include(x => x.DashboardProject)
+                .AsNoTracking();
+
+            if (projectIds.Any())
+            {
+                query = query.Where(i => projectIds.Contains(i.DashboardProject.ProjectId));
+                pageNumber = 1;
+            }
+
+            var orderedQuery = query.OrderByDescending(i => i.DateCreated);
+
+            var totalItems = await orderedQuery.CountAsync();
+
+            var items = await orderedQuery
+                .Skip((pageNumber - 1) * PagingConstants.GalleryPageSize)
+                .Take(PagingConstants.GalleryPageSize)
+            .ToListAsync();
+
+            var itemDTOs = items.Select(x => GalleryHelper.ConvertToDTO(x)).ToList();
+
+            return new PaginatedList<ShowcaseItemDTO>(itemDTOs, totalItems, pageNumber);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}

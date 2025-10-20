1a8840c1c2f183c59fea16878e0fb650d6ba52a8
Dejmenek <dejmen14567@gmail.com>
2025-10-20 23:02:42 +0200

    feat: add gallery page size and project constants
    Added a new constant `GalleryPageSize` to the `PagingConstants` class to support gallery pagination. Introduced a new static class `ProjectConstants` that includes two static readonly lists: `ProjectlessAreas` for various project types and `NonApplicableProjects` for specific project identifiers, enhancing project management capabilities.

--- TCSA.V2026/Helpers/Constants/PagingConstants.cs
+++ TCSA.V2026/Helpers/Constants/PagingConstants.cs
@@ -1,8 +1,9 @@
-﻿namespace TCSA.V2026.Helpers.Constants;
+namespace TCSA.V2026.Helpers.Constants;
 
 public static class PagingConstants
 {
     public const int LeaderboardPageSize = 50;
     public const int ActivityPageSize = 50;
     public const int ChallengesPageSize = 10;
+    public const int GalleryPageSize = 9;
 }
---
+++ TCSA.V2026/Helpers/Constants/ProjectConstants.cs
@@ -0,0 +1,14 @@
+namespace TCSA.V2026.Helpers.Constants;
+
+public static class ProjectConstants
+{
+    public static readonly List<string> ProjectlessAreas = new()
+    {
+        "StandAlone", "FullStack", "StartApplying", "OpenSource", "HowItWorks", "StartHere", "SQL", "Azure"
+    };
+
+    public static readonly List<int> NonApplicableProjects = new()
+    {
+        75, 22, 23, 76, 77, 78, 47, 48, 49, 100000, 86
+    };
+}

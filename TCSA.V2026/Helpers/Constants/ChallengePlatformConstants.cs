304946b77b1c8a3327d933d0b5458abc3c66b455
Dejmenek <dejmen14567@gmail.com>
2026-01-08 22:40:51 +0100

    feat: add helper for generating challenge URLs
    Added ChallengeHelper with GetChallengeUrl method to construct challenge URLs for CodeWars and LeetCode platforms. Introduced ChallengePlatformConstants to store platform base URLs, improving maintainability and consistency when generating challenge links.

---
+++ TCSA.V2026/Helpers/Constants/ChallengePlatformConstants.cs
@@ -0,0 +1,14 @@
+namespace TCSA.V2026.Helpers.Constants;
+
+public static class ChallengePlatformConstants
+{
+    public static class CodeWars
+    {
+        public const string BaseUrl = "https://www.codewars.com/kata/";
+    }
+
+    public static class LeetCode
+    {
+        public const string BaseUrl = "https://leetcode.com/problems/";
+    }
+}
